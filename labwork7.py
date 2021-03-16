1a2b7c70336e706aa7fd68a359061f984945fc56
Safal <safalstha818@gmail.com>
2021-03-16 11:57:42 +0545

    speed, time calculation

--- labwork7.py
+++ labwork7.py
@@ -12,15 +12,20 @@ print(x)'''
 living_miles_apart= 4
 drives_velocity = 25
 time_taken= ((living_miles_apart/drives_velocity)*60)
+
 # 2 mint in each stop
+
 time_spend=20
 total_time= time_taken+time_spend
 print(f'Total time taken by bus is {total_time}')
+
 #jogging
+
 jog_one= ((1/7)*60)
 jog_two= ((1/15)*60)
 jog_three= ((1/7)*60)
 time_walk_time= jog_one+jog_two+jog_three
+
 print(f'time taken by running is {time_walk_time}')
 if (time_walk_time<total_time):
     print('Taking bus is slower tha running')
