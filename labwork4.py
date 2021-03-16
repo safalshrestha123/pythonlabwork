f21758027cf68a3719a53d66d70d4f41de395fb6
Safal <safalstha818@gmail.com>
2021-03-16 11:28:47 +0545

    time calculation

--- labwork4.py
+++ labwork4.py
@@ -7,7 +7,6 @@ print(f'Total day for given seconds:{day}')
 
 hour= ((second//60)//60)
 print(f'Total hour for given seconds:{hour}')
-
 minute= (second//60)
 print(f'Total minute for given second:{minute}')
 
